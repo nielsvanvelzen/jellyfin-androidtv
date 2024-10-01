030dd683f42e50ceccbd1f710ec166c037b2c9b4
Niels van Velzen <git@ndat.nl>
2024-10-01 22:07:42 +0200

    WIP rewrite HomeFragment with compose

--- app/src/main/java/org/jellyfin/androidtv/ui/home/HomeFragment.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/home/HomeFragment.kt
@@ -3,90 +3,151 @@ package org.jellyfin.androidtv.ui.home
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
+import androidx.compose.foundation.focusGroup
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.focusRestorer
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.unit.dp
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.flowWithLifecycle
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
-import org.jellyfin.androidtv.R
-import org.jellyfin.androidtv.auth.repository.ServerRepository
+import androidx.fragment.compose.AndroidFragment
+import androidx.tv.material3.Icon
+import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.NavigationDrawer
+import androidx.tv.material3.NavigationDrawerItem
+import androidx.tv.material3.Surface
+import androidx.tv.material3.Text
+import androidx.tv.material3.darkColorScheme
 import org.jellyfin.androidtv.auth.repository.SessionRepository
-import org.jellyfin.androidtv.auth.repository.UserRepository
-import org.jellyfin.androidtv.data.repository.NotificationsRepository
-import org.jellyfin.androidtv.databinding.FragmentHomeBinding
-import org.jellyfin.androidtv.ui.navigation.Destinations
 import org.jellyfin.androidtv.ui.navigation.NavigationRepository
 import org.jellyfin.androidtv.ui.playback.MediaManager
+import org.jellyfin.androidtv.ui.preference.PreferencesFragment
+import org.jellyfin.androidtv.ui.preference.screen.UserPreferencesScreen
+import org.jellyfin.androidtv.ui.search.SearchFragment
 import org.jellyfin.androidtv.ui.startup.StartupActivity
-import org.jellyfin.androidtv.util.ImageHelper
 import org.koin.android.ext.android.inject
+import org.koin.compose.koinInject
+
+@Composable
+fun HomeScreen(
+	onSwitchUser: () -> Unit,
+) {
+	val navigationRepository = koinInject<NavigationRepository>()
+	val contentFocusRequester = remember { FocusRequester() }
+
+	MaterialTheme(
+		colorScheme = darkColorScheme()
+	) {
+		Surface(
+			modifier = Modifier.focusRestorer(contentFocusRequester),
+		) {
+			var selectedIndex by remember { mutableIntStateOf(0) }
+			val items = listOf(
+				"Home" to Icons.Default.Home,
+				"Favorites" to Icons.Default.Favorite,
+				"Search" to Icons.Default.Search,
+				"Settings" to Icons.Default.Settings,
+			)
+
+			NavigationDrawer(
+				drawerContent = {
+					Column(
+						Modifier
+							.fillMaxHeight()
+							.padding(12.dp)
+							.selectableGroup(),
+						horizontalAlignment = Alignment.Start,
+						verticalArrangement = Arrangement.spacedBy(10.dp)
+					) {
+						items.forEachIndexed { index, item ->
+							val (text, icon) = item
+							NavigationDrawerItem(
+								selected = selectedIndex == index,
+								onClick = { selectedIndex = index },
+								leadingContent = {
+									Icon(
+										imageVector = icon,
+										contentDescription = null,
+									)
+								}
+							) {
+								Text(text)
+							}
+						}
+					}
+				},
+				content = {
+					Box(
+						modifier = Modifier
+							.fillMaxSize()
+							.focusGroup()
+							.focusRequester(contentFocusRequester)
+					) {
+						when (selectedIndex) {
+							0 -> AndroidFragment<HomeRowsFragment>(
+								modifier = Modifier.fillMaxSize(),
+							)
+
+							1 -> {
+								// Fav
+							}
+
+							2 -> AndroidFragment<SearchFragment>(
+								modifier = Modifier.fillMaxSize(),
+							)
+
+							3 -> AndroidFragment<PreferencesFragment>(
+								modifier = Modifier.fillMaxSize(),
+								arguments = bundleOf(
+									PreferencesFragment.EXTRA_SCREEN to UserPreferencesScreen::class.qualifiedName,
+									PreferencesFragment.EXTRA_SCREEN_ARGS to bundleOf()
+								)
+							)
+						}
+					}
+				}
+			)
+		}
+	}
+}
 
 class HomeFragment : Fragment() {
-	private var _binding: FragmentHomeBinding? = null
-	private val binding get() = _binding!!
-
 	private val sessionRepository by inject<SessionRepository>()
-	private val userRepository by inject<UserRepository>()
-	private val serverRepository by inject<ServerRepository>()
-	private val notificationRepository by inject<NotificationsRepository>()
-	private val navigationRepository by inject<NavigationRepository>()
 	private val mediaManager by inject<MediaManager>()
-	private val imageHelper by inject<ImageHelper>()
 
-	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-		_binding = FragmentHomeBinding.inflate(inflater, container, false)
-
-		binding.settings.setOnClickListener {
-			navigationRepository.navigate(Destinations.userPreferences)
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?,
+	) = ComposeView(requireContext()).apply {
+		setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+		setContent {
+			HomeScreen(
+				onSwitchUser = ::switchUser
+			)
 		}
-
-		binding.switchUsers.setOnClickListener {
-			switchUser()
-		}
-
-		binding.search.setOnClickListener {
-			navigationRepository.navigate(Destinations.search())
-		}
-
-		return binding.root
-	}
-
-	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-		super.onViewCreated(view, savedInstanceState)
-
-		userRepository.currentUser
-			.flowWithLifecycle(viewLifecycleOwner.lifecycle, Lifecycle.State.STARTED)
-			.onEach { user ->
-				if (user != null) {
-					binding.switchUsersImage.load(
-						url = imageHelper.getPrimaryImageUrl(user),
-						placeholder = ContextCompat.getDrawable(requireContext(), R.drawable.ic_user)
-					)
-				}
-			}
-			.launchIn(viewLifecycleOwner.lifecycleScope)
-
-		sessionRepository.currentSession
-			.flowWithLifecycle(viewLifecycleOwner.lifecycle, Lifecycle.State.STARTED)
-			.map { session->
-				if (session == null) null
-				else serverRepository.getServer(session.serverId)
-			}
-			.onEach { server ->
-				notificationRepository.updateServerNotifications(server)
-			}
-			.launchIn(viewLifecycleOwner.lifecycleScope)
-	}
-
-	override fun onDestroyView() {
-		super.onDestroyView()
-
-		_binding = null
 	}
 
 	private fun switchUser() {
