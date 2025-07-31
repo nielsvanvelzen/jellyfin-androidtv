1e983e13aca1e51849c0664b4eceffda295bbb6f
Niels van Velzen <git@ndat.nl>
2025-07-31 21:21:08 +0200

    Rewrite home in compose

--- app/src/main/java/org/jellyfin/androidtv/di/AppModule.kt
+++ app/src/main/java/org/jellyfin/androidtv/di/AppModule.kt
@@ -149,7 +149,7 @@ val appModule = module {
 	viewModel { PictureViewerViewModel(get()) }
 	viewModel { SearchViewModel(get()) }
 	viewModel { DreamViewModel(get(), get(), get(), get(), get()) }
-	viewModel { HomeViewModel(get(), get(), get()) }
+	viewModel { HomeViewModel(get(), get(), get(), get()) }
 
 	single { BackgroundService(get(), get(), get(), get(), get()) }
 
