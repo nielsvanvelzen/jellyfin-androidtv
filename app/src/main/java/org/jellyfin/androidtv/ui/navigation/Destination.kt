19093727c6e7235f7db7728b44bcbd5e7bf11874
Niels van Velzen <git@ndat.nl>
2025-11-19 20:57:34 +0100

    Use navigation3

--- app/src/main/java/org/jellyfin/androidtv/ui/navigation/Destination.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/navigation/Destination.kt
@@ -1,16 +1,28 @@
 package org.jellyfin.androidtv.ui.navigation
 
 import android.os.Bundle
+import androidx.compose.runtime.Composable
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation3.runtime.NavKey
 import kotlin.reflect.KClass
 
-sealed interface Destination {
+sealed interface Destination : NavKey {
+	data class ComposableContent(
+		val content: @Composable () -> Unit,
+	) : Destination
+
 	data class Fragment(
 		val fragment: KClass<out androidx.fragment.app.Fragment>,
 		val arguments: Bundle = bundleOf(),
 	) : Destination
 }
+
+fun composeDestination(
+	content: @Composable () -> Unit,
+) = Destination.ComposableContent(
+	content = content,
+)
 
 inline fun <reified T : Fragment> fragmentDestination(
 	vararg arguments: Pair<String, Any?>,
