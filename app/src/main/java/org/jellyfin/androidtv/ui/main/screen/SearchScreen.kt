7e49ef294fd9ec4160190b64549934e9dacefc28
Niels van Velzen <git@ndat.nl>
2026-09-11 22:19:46 +0200

    misc changes

--- app/src/main/java/org/jellyfin/androidtv/ui/main/screen/SearchScreen.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/main/screen/SearchScreen.kt
@@ -1,37 +1,30 @@
 package org.jellyfin.androidtv.ui.main.screen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import org.jellyfin.androidtv.ui.base.Text
 import org.jellyfin.androidtv.ui.base.button.Button
 import org.jellyfin.androidtv.ui.main.Routes
-import org.jellyfin.androidtv.ui.main.composable.PrimaryLayout
 import org.jellyfin.androidtv.ui.navigation.LocalRouter
-import org.jellyfin.androidtv.ui.shared.toolbar.MainToolbarActiveButton
 
 @Composable
-fun SearchScreen() = PrimaryLayout(
-	activeButton = MainToolbarActiveButton.Search,
-) {
+fun SearchScreen() {
 	val router = LocalRouter.current
 
-	Row(
-		modifier = Modifier
-			.fillMaxSize()
-			.background(Color.Magenta)
+	NavigationLayout(
+		navigation = { Navigation() },
 	) {
-		Text("Search page", color = Color.White)
+		Row {
+			Text("Search page", color = Color.White)
 
-		Button(
-			onClick = {
-				router.push(Routes.MAIN)
+			Button(
+				onClick = {
+					router.push(Routes.MAIN)
+				}
+			) {
+				Text("Go to main page")
 			}
-		) {
-			Text("Go to main page")
 		}
 	}
 }
